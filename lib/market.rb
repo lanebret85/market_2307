a766ce127dc1c83711e467db2d3e4bc24c7406e4
Lane Bretschneider <lanebret85@gmail.com>
2023-08-08 10:25:23 -0700

    feat: add total_inventory method to Market class

--- lib/market.rb
+++ lib/market.rb
@@ -26,4 +26,34 @@ class Market
     end
     vendors_selling_item
   end
+
+  def total_inventory
+    market_inventory = Hash.new(0)
+    item_inventory = Hash.new(0)
+    items_sold = []
+    @vendors.each do |vendor|
+      vendor.inventory.keys.each do |inventory_item|
+        items_sold << inventory_item
+      end
+    end
+    
+    unique_items_sold = items_sold.uniq
+    inventory_array = []
+    
+    unique_items_sold.each do |item|
+      item_quantity = 0
+      @vendors.each do |vendor|
+        item_quantity += vendor.check_stock(item)
+      end
+      inventory_array << item_quantity
+    end
+    
+    counter = 0
+    unique_items_sold.each do |item|
+      market_inventory[item] = {:quantity => inventory_array[counter], :vendors => vendors_that_sell(item)}
+      counter += 1
+    end
+    
+    market_inventory
+  end
 end
