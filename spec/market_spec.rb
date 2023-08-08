68c98c006d2ad34248730359891d781ab6284c7c
Lane Bretschneider <lanebret85@gmail.com>
2023-08-08 10:24:57 -0700

    add test for total_inventory method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -74,4 +74,36 @@ RSpec.describe Market do
       expect(market.vendors_that_sell(item4)).to eq([vendor2])
     end
   end
+
+  describe "#total_inventory" do
+    it "can return the quantities of all items sold at the market and the vendors who sell those items" do
+      vendor1 = Vendor.new("Rocky Mountain Fresh")
+      vendor2 = Vendor.new("Ba-Nom-a-Nom")
+      vendor3 = Vendor.new("Palisade Peach Shack")
+
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      item2 = Item.new({name: 'Tomato', price: "$0.50"})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+      vendor1.stock(item1, 35)
+      vendor1.stock(item2, 7)
+
+      vendor2.stock(item4, 50)
+      vendor2.stock(item3, 25)
+
+      vendor3.stock(item1, 65)
+
+      market.add_vendor(vendor1)
+      market.add_vendor(vendor2)
+      market.add_vendor(vendor3)
+
+      expected = {item1 => {:quantity => 100, :vendors => [vendor1, vendor3]},
+                  item2 => {:quantity => 7, :vendors => [vendor1]},
+                  item3 => {:quantity => 25, :vendors => [vendor2]},
+                  item4 => {:quantity => 50, :vendors => [vendor2]}}
+
+      expect(market.total_inventory).to eq(expected)
+    end
+  end
 end
