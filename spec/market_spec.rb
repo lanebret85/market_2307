256b82bc8c2b47b90d951301f974c869a3f755aa
Lane Bretschneider <lanebret85@gmail.com>
2023-08-08 10:45:23 -0700

    test: add test for sorted_item_list method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -133,4 +133,31 @@ RSpec.describe Market do
       expect(market.overstocked_items).to eq([item1])
     end
   end
+
+  describe "#sorted_item_list" do
+    it "can return an array of the names of all items the Vendors ahve in stock sorted alphabetically" do
+      vendor1 = Vendor.new("Rocky Mountain Fresh")
+      vendor2 = Vendor.new("Ba-Nom-a-Nom")
+      vendor3 = Vendor.new("Palisade Peach Shack")
+
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      item2 = Item.new({name: 'Tomato', price: "$0.50"})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+      vendor1.stock(item1, 35)
+      vendor1.stock(item2, 7)
+
+      vendor2.stock(item4, 50)
+      vendor2.stock(item3, 25)
+
+      vendor3.stock(item1, 65)
+
+      market.add_vendor(vendor1)
+      market.add_vendor(vendor2)
+      market.add_vendor(vendor3)
+
+      expect(market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
+    end
+  end
 end
